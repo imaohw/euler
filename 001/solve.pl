09d06cd85266b116b2dc72ff96f2a13c2d00431d
Sebastian Köhler <sebkoehler@whoami.org.uk>
2012-08-01 02:31:31 +0200

    001

---
+++ 001/solve.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $sum = 0;
+
+for(my $i = 0; $i < 1000; $i++) {
+    if( ((($i % 3) == 0) && (($i % 5) == 0)) || ((($i % 5) == 0)) || (($i % 3) == 0) ) {
+        $sum += $i;
+    }
+}
+
+print "$sum\n";
